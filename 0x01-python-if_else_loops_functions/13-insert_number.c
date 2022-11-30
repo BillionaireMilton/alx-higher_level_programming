eb5f2662fdea4990618ee15dea8efc713dd2bb97
BillionaireMilton <henrymilton003@gmail.com>
2022-11-30 01:09:54 +0100

    added list.h and insert number file file on if else loop task

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,42 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_node - inserts a number into a sorted singly linked list.
- * @head: list head
- * @number: number to store in the new node
- * Return: pointer to the new node
+ * insert_node - insert node in an ordered linked list
+ * @head: the list passed in
+ * @number: value of the new node
+ * Return: address of new node or NULL if failure
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *runner;
-        listint_t *new;
+	listint_t *new, *future;
 
-        runner = *head;
+	future = *head;
 
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
 
-        if (*head == NULL || (*head)->n > number)
-        {
-                new->next = *head;
-                *head = new;
-                return (new);
-        }
+	if (*head == NULL || (*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
-        while (runner->next != NULL)
-        {
-                if ((runner->next)->n >= number)
-                {
-                        new->next = runner->next;
-                        runner->next = new;
-                        return (new);
-                }
-                runner = runner->next;
-        }
-
-        new->next = NULL;
-        runner->next = new;
-        return (new);
+	while (future->next != NULL)
+	{
+		if ((future->next)->n >= number)
+		{
+			new->next = future->next;
+			future->next = new;
+			return (new);
+		}
+		future = future->next;
+	}
+	future->next = new;
+	new->next = NULL;
+	return (new);
 }
