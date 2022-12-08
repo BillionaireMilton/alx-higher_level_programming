3f5fb65d959f18f301d335c18007e3ccde6cbefa
BillionaireMilton <henrymilton003@gmail.com>
2022-12-08 19:24:51 +0100

    more data structure task done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,12 @@
-
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
+    """Return the weighted average of all integers in a list of tuples."""
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
 
-    num = 0
-    den = 0
-
-    for wup in my_list:
-        num += wup[0] * wup[1]
-        den += wup[1]
-
-    return (num / den)
+    avg = 0
+    size = 0
+    for tup in my_list:
+        avg += (tup[0] * tup[1])
+        size += tup[1]
+    return (avg / size)
