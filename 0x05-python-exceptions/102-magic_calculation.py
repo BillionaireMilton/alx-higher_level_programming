ed4c1018cde4b0d14ca2f208fc3ba3c9fb3b2157
BillionaireMilton <henrymilton003@gmail.com>
2022-12-20 00:55:58 +0100

    completed python exceptions tasks

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -11,3 +13,4 @@ def magic_calculation(a, b):
             result = b + a
             break
     return (result)
+    
