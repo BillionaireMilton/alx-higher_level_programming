d3997e265369c44ccf5ce931da8d9a8192606f22
BillionaireMilton <henrymilton003@gmail.com>
2023-05-01 02:59:58 +0100

    js task fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
+// lorem ipsum
 
 const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
   }
 });
